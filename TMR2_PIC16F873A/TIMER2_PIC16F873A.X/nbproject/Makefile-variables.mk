0e973a72bad441dc1c315b876ead860525c2826b
Carlos Quispe <carlos359_7@hotmail.com>
2022-03-19 21:14:17 -0500

    Add files via upload

---
+++ TMR2_PIC16F873A/TIMER2_PIC16F873A.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=TIMER2_PIC16F873A.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/TIMER2_PIC16F873A.X.production.hex
